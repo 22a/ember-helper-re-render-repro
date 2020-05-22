726f5f371ca4a559f656d99f23fc04c800518d11
Peter Meehan <peter.meehan@intercom.io>
2020-05-22 14:04:25 +0100

    enduce re-render on click, throw if event.target not attached to DOM in document click handler

--- app/components/my-component.js
+++ app/components/my-component.js
@@ -13,11 +13,22 @@ export default Component.extend({
     document.removeEventListener('click', this.handleDocumentClick);
   },
 
+  click() {
+    // eslint-disable-next-line no-console
+    console.log(`some action that forces our {{each}} subject to update before
+    the click event propagates to our document click handler below`);
+    this.set('iconNames', this.iconNames.slice());
+  },
+
   handleDocumentClick: action(function(event) {
+    let eventTargetIsAttachedToDOM = Boolean(event.target.closest('html'));
     // eslint-disable-next-line no-console
     console.log({
       eventTarget: event.target,
-      eventTargetIsAttachedToDOM: Boolean(event.target.closest('html')),
+      eventTargetIsAttachedToDOM,
     })
+    if (!eventTargetIsAttachedToDOM) {
+      throw new Error('event target no longer attached to the DOM');
+    }
   }),
 });
--- tests/integration/components/my-component-test.js
+++ tests/integration/components/my-component-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | my-component', function(hooks) {
@@ -9,5 +9,11 @@ module('Integration | Component | my-component', function(hooks) {
   test('it renders', async function(assert) {
     await render(hbs`<MyComponent />`);
     assert.equal(this.element.textContent.trim(), '');
+  });
+
+  test('it doesn\'t blow up when clicking on an inline svg', async function(assert) {
+    await render(hbs`<MyComponent />`);
+    await click('svg');
+    assert.ok(true);
   });
 });
